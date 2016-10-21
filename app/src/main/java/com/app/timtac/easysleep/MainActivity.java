32c05c83cb3398234e2ccc669e01705486a8d6e5
Timothy <titidep001@hotmail.com>
2016-10-21 15:52:49 +0200

    Degraded ok

--- app/src/main/java/com/app/timtac/easysleep/MainActivity.java
+++ app/src/main/java/com/app/timtac/easysleep/MainActivity.java
@@ -4,90 +4,148 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.RemoteException;
 import android.provider.Settings;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private boolean AUTO_HIDE = true;
-
     // UI
     private View root;
-    private ImageButton start;
+    private ImageView start;
     private LinearLayout linearLayout;
+
+    // Variables
+    private Context mContext;
+    private int brightness_saved;
+    private String color_bg = "#D73612";
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mContext = getApplicationContext();
 
-        linearLayout = (LinearLayout)findViewById(R.id.backgroundButton);
-        start = (ImageButton)findViewById(R.id.playButton);
+        linearLayout = (LinearLayout) findViewById(R.id.backgroundButton);
+        start = (ImageView) findViewById(R.id.playButton);
         root = linearLayout.getRootView();
 
+        // Get brightness to store it
+        brightness_saved = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.SCREEN_BRIGHTNESS, 0);
+
+
+        // Start light when button is touched
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                root.setBackgroundColor(Color.parseColor("#D73612"));
 
+                root.setBackgroundColor(Color.parseColor(color_bg));
+
+                // Hide action bar
+                ActionBar actionBar = getSupportActionBar();
+                if (actionBar != null) {
+                    actionBar.hide();
+                }
+
+                // Hide start button
+                start.setVisibility(View.GONE);
+
+
+                // Set birghtness to maximum
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    if (Settings.System.canWrite(getApplicationContext())) {
-                        Settings.System.putInt(
-                                getApplicationContext().getContentResolver(),
-                                Settings.System.SCREEN_BRIGHTNESS,
-                                255
+                    if (Settings.System.canWrite(mContext)) {
+                        Settings.System.putInt(mContext.getContentResolver(),
+                                Settings.System.SCREEN_BRIGHTNESS, 255
                         );
-                    }
-                    else {
+                    } else {
                         Intent intent = new Intent(android.provider.Settings.ACTION_MANAGE_WRITE_SETTINGS);
                         intent.setData(Uri.parse("package:" + MainActivity.this.getPackageName()));
                         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                         startActivity(intent);
                     }
                 }
-
-                ActionBar actionBar = getSupportActionBar();
-                if (actionBar != null) {
-                    actionBar.hide();
+                else
+                {
+                    Settings.System.putInt(getApplicationContext().getContentResolver(),
+                            Settings.System.SCREEN_BRIGHTNESS, 255);
                 }
 
+                StartBrightnessDown();
 
             }
         });
 
     }
 
+    public void StartBrightnessDown()
+    {
+        new DowngradeBrightness().execute(50);
+
+    }
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+    public void onPause()
+    {
+        super.onPause();
+
+        // Shows Action bar
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.show();
+        }
+
+        //set Brightness to same as before when quitting app
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            if (Settings.System.canWrite(mContext))
+            {
+                Settings.System.putInt(mContext.getContentResolver(),
+                        Settings.System.SCREEN_BRIGHTNESS, brightness_saved);
+            }
+            else
+            {
+                Intent intent = new Intent(android.provider.Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                intent.setData(Uri.parse("package:" + MainActivity.this.getPackageName()));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        }
+        else
+        {
+            Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.SCREEN_BRIGHTNESS, brightness_saved);
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -97,4 +155,35 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public class DowngradeBrightness extends AsyncTask<Integer, Void, Void>
+    {
+        @Override
+        protected Void doInBackground(Integer... params)
+        {
+            int duration = params[0];
+
+            for (int i=255 ; i>0 ; i--)
+            {
+                try
+                {
+                    Thread.sleep(duration);
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+
+                Settings.System.putInt(mContext.getContentResolver(),
+                        Settings.System.SCREEN_BRIGHTNESS, i);
+            }
+            return null;
+        }
+
+        protected void onPostExecute()
+        {
+            Toast.makeText(mContext,"DONE",Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
